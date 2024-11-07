4a2ff343a6717f42ff7aaf362661d100fed85f0f
Acors24 <acors@protonmail.com>
2024-11-07 10:00:01 +0100

    add transition for indicators

--- src/components/GuessIndicatorBar.tsx
+++ src/components/GuessIndicatorBar.tsx
@@ -47,7 +47,7 @@ function GuessIndicator({
 
   return (
     <span
-      className={`w-3 h-3 rounded-full inline-block shadow-glow ${className}`}
+      className={`w-3 h-3 rounded-full inline-block shadow-glow duration-200 ${className}`}
     ></span>
   );
 }
