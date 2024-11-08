787976c3d72332f7a651bfdbc1bb60bfd4328908
Acors24 <acors@protonmail.com>
2024-11-08 15:08:19 +0100

    fix gh pages 404 when refreshing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const hashHistory = createHashHistory();
+
 // Create a new router instance
-const router = createRouter({ routeTree, basepath: "/hoyo-mixle" });
+const router = createRouter({
+  routeTree,
+  // basepath: "/hoyo-mixle",
+  history: hashHistory,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
