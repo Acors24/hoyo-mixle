b682abb4c3404262618c04cd17dd455f12867d9c
Acors24 <acors@protonmail.com>
2025-05-04 13:26:26 +0200

    add missing Fandom links for some songs

--- src/components/Changelog.tsx
+++ src/components/Changelog.tsx
@@ -4,6 +4,10 @@ import Dialog from "./Dialog";
 
 const history = [
   {
+    date: "2025-05-04",
+    changes: ["Added missing Fandom links for some songs"],
+  },
+  {
     date: "2025-05-01",
     changes: ["Optimized calendar logic"],
   },
