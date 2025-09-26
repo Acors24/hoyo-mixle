0138ab9718ae31d64d0936b7dd0df15e3069e3ff
Acors24 <acors@protonmail.com>
2025-09-26 23:29:47 +0200

    add FURY ON album

--- src/components/Changelog.tsx
+++ src/components/Changelog.tsx
@@ -4,6 +4,10 @@ import Dialog from "./Dialog";
 
 const history = [
   {
+    date: "2025-09-26",
+    changes: ['Added "FURY ON" album (Zenless Zone Zero)'],
+  },
+  {
     date: "2025-08-23",
     changes: ['Added "The Shimmering Voyage Vol. 5" album (Genshin Impact)'],
   },
